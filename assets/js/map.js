a1ae2a1a4cfe229f373331c176ea350961b49311
CheekiBreeki <223176694+Cheekitos@users.noreply.github.com>
2025-09-30 16:39:59 -0400

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,53 +6,56 @@
 
   window.StalkerMap = {
     toggleEngine: function(engineCard) {
-      const container = engineCard.querySelector('.children-container');
-      const icon = engineCard.querySelector('.engine-header .expand-icon');
+      const branch = engineCard.closest('.mod-branch');
+      const container = branch.querySelector('.children-container');
+      const icon = engineCard.querySelector('.expand-icon');
+      const details = engineCard.querySelector('.mod-details');
       
       if (engineCard.classList.contains('expanded')) {
         // Collapse engine
         engineCard.classList.remove('expanded');
         container.classList.remove('expanded');
         icon.classList.remove('rotated');
+        details.classList.remove('expanded');
       } else {
         // Expand engine
         engineCard.classList.add('expanded');
         container.classList.add('expanded');
         icon.classList.add('rotated');
+        details.classList.add('expanded');
       }
     },
 
-    toggleChild: function(childCard) {
-      const details = childCard.querySelector('.child-details');
-      const icon = childCard.querySelector('.child-header .expand-icon');
-      
-      if (childCard.classList.contains('expanded')) {
-        // Collapse child
-        details.style.height = details.scrollHeight + 'px';
-        requestAnimationFrame(() => {
-          details.style.height = '0';
-          details.classList.remove('expanded');
-          icon.classList.remove('rotated');
-          childCard.classList.remove('expanded');
-        });
-      } else {
-        // Expand child
-        childCard.classList.add('expanded');
-        icon.classList.add('rotated');
-        details.classList.add('expanded');
-        const height = details.scrollHeight;
-        details.style.height = '0';
-        requestAnimationFrame(() => {
-          details.style.height = height + 'px';
-        });
-        
-        // Remove inline height after transition completes
-        setTimeout(() => {
-          if (details.classList.contains('expanded')) {
-            details.style.height = 'auto';
-          }
-        }, 350);
+    toggleCard: function(card) {
+      const details = card.querySelector('.mod-details');
+      const icon = card.querySelector('.expand-icon');
+      if (card.classList.contains('expanded')) {
+    // Collapse card
+    details.style.height = details.scrollHeight + 'px';
+    requestAnimationFrame(() => {
+      details.style.height = '0';
+      details.classList.remove('expanded');
+      icon.classList.remove('rotated');
+      card.classList.remove('expanded');
+    });
+  } else {
+    // Expand card
+    card.classList.add('expanded');
+    icon.classList.add('rotated');
+    details.classList.add('expanded');
+    const height = details.scrollHeight;
+    details.style.height = '0';
+    requestAnimationFrame(() => {
+      details.style.height = height + 'px';
+    });
+    
+    // Remove inline height after transition completes
+    setTimeout(() => {
+      if (details.classList.contains('expanded')) {
+        details.style.height = 'auto';
       }
-    }
-  };
+    }, 350);
+  }
+}
+    };
 })();
